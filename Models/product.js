c8d4448051d912cdaeb0e9e09d51711cc8602861
Tharanga Kasthuriarachchi <gamesfreak26@gmail.com>
2021-03-14 18:36:54 +1000

    Make string array an enum

--- Models/product.js
+++ Models/product.js
@@ -6,8 +6,8 @@ const Product = new Schema({
     description: String,
     ownerId: { type: Schema.Types.ObjectId, ref: 'Owner' },
     price: { type: Number, default: 0 },
-    category: [String],
-    currentCondition: { type: Number, default: 0 },
+    category: { type: String, enum: ['Mobility', 'Hygiene', 'Apparel', 'Bedroom', 'Dining'], required: true },
+    currentCondition: { type: String, enum: ['Poor', 'Fair', 'Good', 'Very Good', 'Excellent'], required: true },
     brand: String
 })
 
